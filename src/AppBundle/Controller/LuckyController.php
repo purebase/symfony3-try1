3a28b07b3125dcc5891ce3c1d9757b1e304a8a37
Thomas Eilermann <user@purebase.de>
2016-09-02 20:33:21 +0200

    Added validated routing example.

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -11,6 +11,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class LuckyController extends Controller
 {
@@ -26,14 +27,16 @@ class LuckyController extends Controller
      *     }
      *  )
      */
-    public function numberAction($from, $year)
+    public function numberAction($from, $year, Request $request)
     {
         $number = mt_rand(0, 100);
+        $request_search = $request->query->get('search');
 
         return $this->render('lucky/number.html.twig', array(
             'callFromName' => $from,
             'year' => $year,
-            'number' => $number
+            'number' => $number,
+            'search' => $request_search
         ));
     }
 }
