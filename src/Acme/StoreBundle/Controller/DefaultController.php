f53676ca05fb62fae6130bcfad421642aade5eb2
Thomas Eilermann <user@purebase.de>
2016-09-03 14:41:18 +0200

    Added bundle to try the new doctrine-mongodb functionality.

---
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Acme\StoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Acme\StoreBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AcmeStoreBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route("/product/create")
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
+        return new Response('Created product id '.$product->getId());
+    }
+
+    /**
+     * @Route("/product/show/{id}")
+     */
+    public function showAction($id)
+    {
+        $product = $this->get('doctrine_mongodb')
+            ->getRepository('AcmeStoreBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        return new Response('Found product id '.$product->getName());
+    }
+
+}
